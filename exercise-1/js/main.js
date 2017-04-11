cbccb78a5e17784f87d54aba901435fd4799102b
Tianai Zhao <zhang201pig@126.com>
2017-04-11 11:06:43 -0700

    Add code

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -4,13 +4,30 @@
 $(function() {
 
   // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript varaible
+  var appendDiv = d3.select("#sandbox").append("div").attr('id', 'my-div');
 
   // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
+  var mySvg = appendDiv.append("svg");
 
   // Set both the width and height attributes of your `svg` to 300 
+  mySvg.attr('height', 300)
+  	.attr('width', 300);
 
   // Append a `circle` element to your `svg`, setting the properties noted in index.html
+  mySvg.append("circle")
+	.attr("r", 30)
+	.attr("cx", 75)
+	.attr('cy', 30)
+	.style('opacity', .5)
+	.style('fill', 'blue');
 
   // Append a `rect` element to your `svg`, setting the properties noted in index.html
-
+  mySvg.append("rect")
+	.attr("r", 30)
+	.attr("x", 70)
+	.attr('y', 60)
+	.attr('height', 250)
+	.attr('width', 10)
+	.style('opacity', .5)
+	.style('fill', 'orange');
 });
